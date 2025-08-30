1e6d1fda33bfc68376f29af82e7589ac21ba83dd
Takahiro Kakisu <takahirokakisu@gmail.com>
2025-08-30 20:06:33 +0900

    フォトライブラリー機能を追加

--- MyCamera/ContentView.swift
+++ MyCamera/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
     // 撮影した写真を保持する状態変数
@@ -13,6 +14,9 @@ struct ContentView: View {
     
     // 撮影画面（sheet）の開閉状態を管理
     @State var isShowSheet = false
+    
+    // フォトライブラリーで選択した写真を管理
+    @State var photoPickerSelectedImage: PhotosPickerItem? = nil
     
     var body: some View {
         VStack {
@@ -52,6 +56,35 @@ struct ContentView: View {
                 // UIImagePickerController（写真撮影）を表示
                 ImagePickerView(isShowSheet: $isShowSheet, captureImage: $captureImage)
             }
+            
+            // フォトライブラリーから選択する
+            PhotosPicker(selection: $photoPickerSelectedImage, matching: .images, preferredItemEncoding: .automatic, photoLibrary: .shared()) {
+                
+                Text("フォトライブラリーから選択する")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .background(Color.blue)
+                    .foregroundStyle(Color.white)
+                    .padding()
+            }
+            // 選択した写真情報を元に写真を取り出す
+            .onChange(of: photoPickerSelectedImage, initial: true, { oldValue, newValue in
+                // 選択した写真があるとき
+                if let newValue {
+                    newValue.loadTransferable(type: Data.self) { result in
+                        switch result {
+                        case .success(let data):
+                            // 写真があるとき
+                            if let data {
+                                // 写真をcaptureImageに保存
+                                captureImage = UIImage(data: data)
+                            }
+                        case .failure:
+                            return
+                        }
+                    }
+                }
+            })
             
             // captureImageをアンラップする
             if let captureImage {
