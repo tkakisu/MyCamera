8101837cd04fa6daab2962f45909cc713ced05b0
Takahiro Kakisu <takahirokakisu@gmail.com>
2025-09-01 00:36:56 +0900

    エフェクト機能を追加

--- MyCamera/ContentView.swift
+++ MyCamera/ContentView.swift
@@ -22,22 +22,16 @@ struct ContentView: View {
         VStack {
             Spacer()
             
-            // 撮影した写真があるとき
-            if let captureImage {
-                // 撮影写真を表示
-                Image(uiImage: captureImage)
-                    .resizable()
-                    .scaledToFit()
-            }
-            
-            Spacer()
-            
             // 「カメラを起動する」ボタン
             Button {
                 // カメラが利用可能かチェック
                 if UIImagePickerController.isSourceTypeAvailable(.camera) {
                     print("カメラは利用できます")
                     
+                    // 撮影写真を初期化する
+                    captureImage = nil
+                    
+                    // カメラが使えるなら、isShowSheetをtrue
                     isShowSheet.toggle()
                 } else {
                     print("カメラは利用できません")
@@ -53,8 +47,13 @@ struct ContentView: View {
             }
             .padding()
             .sheet(isPresented: $isShowSheet) {
-                // UIImagePickerController（写真撮影）を表示
-                ImagePickerView(isShowSheet: $isShowSheet, captureImage: $captureImage)
+                if let captureImage {
+                    // 撮影した写真がある→EffectViewを表示する
+                    EffectView(isShowSheet: $isShowSheet, captureImage: captureImage)
+                } else {
+                    // UIImagePickerController（写真撮影）を表示
+                    ImagePickerView(isShowSheet: $isShowSheet, captureImage: $captureImage)
+                }
             }
             
             // フォトライブラリーから選択する
@@ -85,24 +84,14 @@ struct ContentView: View {
                     }
                 }
             })
-            
-            // captureImageをアンラップする
-            if let captureImage {
-                // captureImageから共有する画像を生成する
-                let shareImage = Image(uiImage: captureImage)
-                
-                // 共有シート
-                ShareLink(item: shareImage, subject: nil, message: nil, preview: SharePreview("Photo", image: shareImage)) {
-                    
-                    Text("SNSに投稿する")
-                        .frame(maxWidth: .infinity)
-                        .frame(height: 50)
-                        .background(Color.blue)
-                        .foregroundStyle(Color.white)
-                        .padding()
-                }
-            }
         }
+        // 撮影した写真を保持する状態変数が変化したら実行する
+        .onChange(of: captureImage, initial: true, { oldValue, newValue in
+            if let _ = newValue {
+                // 撮影した写真がある→EffectViewを表示する
+                isShowSheet.toggle()
+            }
+        })
     }
 }
 
---
+++ MyCamera/EffectView.swift
@@ -0,0 +1,124 @@
+//
+//  EffectView.swift
+//  MyCamera
+//
+//  Created by takahiro kakisu on 2025/08/30.
+//
+
+import SwiftUI
+
+struct EffectView: View {
+    // エフェクト編集画面(sheet)の開閉状態を管理
+    @Binding var isShowSheet: Bool
+    // 撮影した写真
+    let captureImage: UIImage
+    // 表示する写真
+    @State var showImage: UIImage?
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            if let showImage {
+                // 表示する写真がある場合は画面に表示
+                Image(uiImage: showImage)
+                    .resizable()
+                    .scaledToFit()
+            }
+            
+            Spacer()
+            
+            Button {
+                // フィルタ名を指定
+                let filterName = "CIPhotoEffectMono"
+                // 元々の画像の回転角度を取得
+                let rotate = captureImage.imageOrientation
+                // UIImage形式の画像をCIImage形式に変換
+                let inputImage = CIImage(image: captureImage)
+                
+                // フィルタ名を指定してCIFilterのインスタンスを取得
+                guard let effectFilter = CIFilter(name: filterName) else {
+                    return
+                }
+                
+                // フィルタ加工のパラメータを初期化
+                effectFilter.setDefaults()
+                
+                // インスタンスにフィルタ加工する元画像を設定
+                effectFilter.setValue(inputImage, forKey: kCIInputImageKey)
+                
+                // フィルタ加工を行う情報を生成
+                guard let outputImage = effectFilter.outputImage else {
+                    return
+                }
+                
+                // CIContentのインスタンスを取得
+                let ciContext = CIContext(options: nil)
+                
+                // フィルタ加工後の画像をCIContext上に描画し、
+                // 結果をcgImageとしてCGImage形式の画像を取得
+                guard let cgImage = ciContext.createCGImage(outputImage, from: outputImage.extent) else {
+                    return
+                }
+                
+                // フィルタ加工後の画像をCGImage形式から
+                // UIImage形式に変更。その際に回転角度を指定。
+                showImage = UIImage(
+                    cgImage: cgImage,
+                    scale: 1.0,
+                    orientation: rotate)
+            } label: {
+                Text("エフェクト")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .multilineTextAlignment(.center)
+                    .background(Color.blue)
+                    .foregroundStyle(Color.white)
+            }
+            .padding()
+            
+            if let showImage {
+                // captureImageから共有する画像を生成する
+                let shareImage = Image(uiImage: showImage)
+                
+                // 共有シート
+                ShareLink(item: shareImage, subject: nil, message: nil, preview: SharePreview("photo", image: shareImage)) {
+                    
+                    Text("シェア")
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 50)
+                        .background(Color.blue)
+                        .foregroundStyle(Color.white)
+                }
+                .padding()
+            }
+            
+            // 「閉じる」ボタン
+            Button {
+                // ボタンをタップした時のアクション
+                // エフェクト編集画面を閉じる
+                isShowSheet.toggle()
+            } label: {
+                Text("閉じる")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .multilineTextAlignment(.center)
+                    .background(Color.blue)
+                    .foregroundStyle(Color.white)
+            }
+            .padding()
+        }
+        // 写真が表示されるときに実行される
+        .onAppear {
+            // 撮影した写真を表示する写真に設定
+            showImage = captureImage
+        }
+    }
+}
+
+#Preview {
+    EffectView(
+        isShowSheet: .constant(true),
+        captureImage: UIImage(named: "preview_use")!
+    )
+}
